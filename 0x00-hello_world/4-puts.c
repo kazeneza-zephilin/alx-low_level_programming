b0ee096fc3df24a189c42c5e39cacb6a02f8118b
kazeneza-zephilin <kzephilin@gmail.com>
2023-06-15 16:37:55 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /*
- * main returns zero for success
- * display statement using puts
- */
+* main returns zero for success
+*display statement using puts
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-    return 0;
-}
+    return 0;}
