d4d2fec0c98b68f7bda6f998e33027659951dd86
kazeneza-zephilin <kzephilin@gmail.com>
2023-06-15 15:58:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
-#include<stdio.h>
-/*
-* main - Entry point
-*/
-int main(void)
+#include <stdio.h>
+int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+puts("Programming is like building a multilingual puzzle");
+	return 0;
 }
