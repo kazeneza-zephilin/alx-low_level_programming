cd7471d3214734a8f773ea20c753791e9e7fa36a
kazeneza-zephilin <kzephilin2@gmail.com>
2023-07-31 13:41:29 -0700

    Sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * sum_listint vi- calculates the sum of all the data in a listint_t list
- * @head: first node in the linked list
+ *sum_listint - calculates the sum of all the data in a listint_t list
+ *@head: first node in the linked list
  *
- * Return: resulting sum
+ *Return: resulting sum
  */
 int sum_listint(listint_t *head)
 {
