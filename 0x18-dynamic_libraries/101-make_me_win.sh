e4c433e0a3a5492c434ffaddbcf774507cb4324a
kazeneza-zephilin <kzephilin2@gmail.com>
2023-10-16 22:48:54 +0200

    updates

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /temp https://github.com/kazeneza-zephilin/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib_printf.so
-export LD_PRELOAD=/temp/lib_printf.so ./gm 9 8 10 24 75 9
+export LD_PRELOAD=/temp/lib_printf.so
