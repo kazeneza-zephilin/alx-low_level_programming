3e20bc752ff28a9995764237ce68b7ce284499eb
kazeneza-zephilin <kzephilin2@gmail.com>
2023-10-16 16:46:01 +0200

    updates

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # collect all .c files in the current directory and compile into shared library.
-c_files =$(ls *.c)
-gcc -shared -fPIC -o liball.so $c_files
+gcc -fPIC *.c
+gcc -shared -o liball.so *.o
